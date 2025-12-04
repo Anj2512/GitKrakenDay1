e98e795c5aff698e5b1841a2b3e9e1ef9d310434
Anj2512 <joshianvi9@gmail.com>
2025-12-03 23:54:07 -0500

    Third Commit - Stored the totals from each of the thread runs.

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Thread[] threads = new Thread[1000];
 
-        long total = 0;
+        long isd = 0;
+        long[] totals  = new long[1000];
         for (int i = 0; i < threads.length; i++) {
+            final int index = i; // needed for inner classes
+
             threads[i] = new Thread(new Runnable(){
                 public void run() {
                     long sum = 0;
                     for(int j = 1; j <= 1000000; j++) {
                         sum += j;
                     }
+                    totals[index] = sum;
                 }
             });
         }
